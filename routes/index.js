9f0577658e10a6e04a1cffabf3b7532a543fc146
safwan <safuswafvan1@gmail.com>
2024-05-27 19:27:07 +0530

    Changed renamed model folder to controllers

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mailModel = require('../models/email.js');
+const mailModel = require('../controllers/email.js');
 const routes = express.Router()
 
 routes.post('/sendMail',mailModel)
